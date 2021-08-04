82789ee660d3efdbf883c8166a9b238377c19940
Ragnaroekk <rayzor007@gmail.com>
2021-08-03 21:20:51 -0700

    changes to serialization

--- insecure-deserialization/index.php
+++ insecure-deserialization/index.php
@@ -1,45 +1,73 @@
-<!-- Simpe login page from: https://makitweb.com/create-simple-login-page-with-php-and-mysql/-->
+<!DOCTYPE html>
+<html>
+<title> CS467 Insecure Deserialization </title>
+<head>
+ <meta name="author" content="Ray Franklin">
+ <meta name="Reference source" 
+ content=https://blog.convisoappsec.com/en/introduction-to-insecure-deserialization-in-php/
+ content="https://makitweb.com/create-simple-login-page-with-php-and-mysql/">
+</head>
+<body>
+<h1 align="center"> Employee Class Serialized:</h1>
+<br>
 
-<div class="container">
-    <form method="post" action="">
-        <div id="div_login">
-            <h1>Login</h1>
-            <div>
-                <input type="text" class="textbox" id="txt_uname" name="txt_uname" placeholder="Username" />
-            </div>
-            <div>
-                <input type="password" class="textbox" id="txt_uname" name="txt_pwd" placeholder="Password"/>
-            </div>
-            <div>
-                <input type="submit" value="Submit" name="but_submit" id="but_submit" />
-            </div>
-        </div>
-    </form>
-</div>
+<table align="center">
+<tr><td>
+<form action="vulnerable.php" method="get">
+	<input type="text" name="username" placeholder="Username" />
+	<input type="submit" value="Search" />
+</form>
+</td></tr>
+</table>
+<table align="center">
+<tr><td>
+<form action="vulnerable.php" method="get">
+	<input type="text" name="danger" placeholder="Danger" />
+	<input type="submit" value="Danger" />
+</form>
+</td></tr>
+</table>
 
 <?php
-include "config.php";
 
-if(isset($_POST['but_submit'])){
+class User{
+    public $user_name;
+    public $is_admin;
 
-    $uname = mysqli_real_escape_string($con,$_POST['txt_uname']);
-    $password = mysqli_real_escape_string($con,$_POST['txt_pwd']);
-
-    if ($uname != "" && $password != ""){
-
-        $sql_query = "select count(*) as cntUser from test_table where username='".$uname."' and pwd='".$password."'";
-        $result = mysqli_query($con,$sql_query);
-        $row = mysqli_fetch_array($result);
-
-        $count = $row['cntUser'];
-
-        if($count > 0){
-            $_SESSION['uname'] = $uname;
-            header('Location: home.php');
-        }else{
-            echo "Invalid username and password";
+    public function DisplayAdminStatus(){
+        if ($this->is_admin){
+            echo $this->$user_name . " is an admin ";
+        } else {
+            echo $this->$user_name . " is a regular user";
         }
-
     }
 
 }
+
+if (isset($_REQUEST["username"])) {
+$object = new User();
+$object->user_name = $_GET["username"];
+$object->is_admin = FALSE;
+
+echo '<div align="center">';
+echo "User name: " . $object->user_name . ", ";
+echo $object->DisplayAdminStatus();
+echo "<br>";
+echo "Serialized data: " . serialize($object);
+echo '</div>';
+}
+
+if (isset($_REQUEST["danger"])) {
+$danger_object = unserialize($_GET['danger']);
+echo '<div align="center">';
+echo "<br>";
+echo "User name: " . $danger_object->user_name . ", ";
+echo $danger_object->DisplayAdminStatus();
+echo "<br>";
+echo "Serialized data: " . serialize($danger_object);
+echo '</div>';
+}
+
+?>
+
+
