999220df6c8d7fff0bb033a619823f6343673e7c
Ragnaroekk <rayzor007@gmail.com>
2021-08-05 13:19:03 -0700

    adding detail to #9

--- known-vulnerabilities/index.php
+++ known-vulnerabilities/index.php
@@ -7,14 +7,24 @@
 <body>
 <h1 align="center"> Using Components with Known Vulnerabilities</h1>
 <br>
+<div align="center">
+This site was purpose built to display know vulnerabilities. <br>
+It is <strong>not</strong> secure and should <strong>not</strong> be hosted publically. <br>
 
-We built this site with Apache Web Server and MySQL. 
-Current number of publicly disclosed cybersecurity vulnerabilities vulnerabilites for Apache: 1874
-Current number of publicly disclosed cybersecurity vulnerabilities vulnerabilites for mySQL: 1432
-
-We used 
+</div>
+<div align="center">
+We built this site with XAMPP, Apache Web Server, and MySQL. <br>
+Current number of publicly disclosed cybersecurity vulnerabilities for XAMPP: 17 <br>
+Current number of publicly disclosed cybersecurity vulnerabilities for mySQL: 1432 <br>
+Current number of publicly disclosed cybersecurity vulnerabilities for Apache: 1874 <br>
+</div>
+<div align="center"><br>
+We used PHP, Javascript, and HTML.
+Current number of publicly disclosed cybersecurity vulnerabilities for Javascript: 3465 <br>
+Current number of publicly disclosed cybersecurity vulnerabilities for PHP: 6960 <br>
+Current number of publicly disclosed cybersecurity vulnerabilities for HTML: 13,307 <br>
 
 Source: <a href="https://cve.mitre.org/cve/">CVE List Home</a>
-
-
+<div>
+</body>
 </html>
