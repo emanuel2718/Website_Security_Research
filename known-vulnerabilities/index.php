7a9f79c4b9e0604355b211d37f6684d26aecb221
Ragnaroekk <rayzor007@gmail.com>
2021-08-05 13:04:31 -0700

    Working on #9

---
+++ known-vulnerabilities/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<title> CS467 Using Components with Known Vulnerabilities</title>
+<head>
+ <meta name="author" content="Ray Franklin">
+</head>
+<body>
+<h1 align="center"> Using Components with Known Vulnerabilities</h1>
+<br>
+
+We built this site with Apache Web Server and MySQL. 
+Current number of publicly disclosed cybersecurity vulnerabilities vulnerabilites for Apache: 1874
+Current number of publicly disclosed cybersecurity vulnerabilities vulnerabilites for mySQL: 1432
+
+We used 
+
+Source: <a href="https://cve.mitre.org/cve/">CVE List Home</a>
+
+
+</html>
