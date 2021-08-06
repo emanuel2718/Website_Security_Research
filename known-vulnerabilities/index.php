1055c134cb07ae3d5d016c62b5470f3fb3a83955
Ragnaroekk <rayzor007@gmail.com>
2021-08-05 17:27:32 -0700

    format changes

--- known-vulnerabilities/index.php
+++ known-vulnerabilities/index.php
@@ -8,18 +8,18 @@
 <h1 align="center"> Using Components with Known Vulnerabilities</h1>
 <br>
 <div align="center">
-This site was purpose built to display know vulnerabilities. <br>
+This site was purpose built to display known vulnerabilities. <br>
 It is <strong>not</strong> secure and should <strong>not</strong> be hosted publically. <br>
 
 </div>
-<div align="center">
+<div align="center"><br>
 We built this site with XAMPP, Apache Web Server, and MySQL. <br>
 Current number of publicly disclosed cybersecurity vulnerabilities for XAMPP: 17 <br>
 Current number of publicly disclosed cybersecurity vulnerabilities for mySQL: 1432 <br>
 Current number of publicly disclosed cybersecurity vulnerabilities for Apache: 1874 <br>
 </div>
 <div align="center"><br>
-We used PHP, Javascript, and HTML.
+We used PHP, Javascript, and HTML.<br>
 Current number of publicly disclosed cybersecurity vulnerabilities for Javascript: 3465 <br>
 Current number of publicly disclosed cybersecurity vulnerabilities for PHP: 6960 <br>
 Current number of publicly disclosed cybersecurity vulnerabilities for HTML: 13,307 <br>
